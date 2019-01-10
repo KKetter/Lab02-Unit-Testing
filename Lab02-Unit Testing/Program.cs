5d19fd9205d34920e628591070f4cf07da9baae8
Kent Ketter <kent.ketter1@gmail.com>
2019-01-09 21:34:49 -0800

    I TOOK A BREAK AND DIDNT SWITCH BRANCHES LIKE I INTENDED TO WHEN I GOT BACK .fail

--- Lab02-Unit Testing/Program.cs
+++ Lab02-Unit Testing/Program.cs
@@ -4,15 +4,17 @@ namespace Lab02_Unit_Testing
 {
     public class Program
     {
+        public static double Balance = 500;
         static void Main(string[] args)
         {
             //Display Application Menu including logo and 4 functions
             Menu();
-            //ATMFunction();
+            ATMFunction();
             Console.ReadLine();
 
 
         }
+        // Create ATM Logo and Options
         public static void Menu()
         {
             Console.WriteLine(@"
@@ -27,21 +29,39 @@ namespace Lab02_Unit_Testing
 (4) Exit
 ");
         }
-        public static void MenuOptions(int menuSelection)
+        // 
+        public static void ATMFunction(int menuSelection)
         {
             switch (menuSelection)
             {
                 // Display Balance
-                case 0:
-                    DisplayBalance(Balance);
+                case 1:
+                    //DisplayBalance(Balance);
                     Console.ReadKey();
                     Console.Clear();
                     break;
+
                 // Deposit Money
-                case 1:
+                case 2:
+                    
                     Console.ReadKey();
-                
-                // Exit Method
+                    Console.Clear();
+                    break;
+
+                // Withdraw Money
+                case 3:
+
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                // Exit App
+                case 4:
+                    //ExitATM();
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
                 default:
                     Console.Write("Apologies we didnt catch that. Please try again. \nPress any key to continue.");
                     Console.ReadKey();
