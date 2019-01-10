5768eec3f5de3250b86ebecba8a703ae0d651750
Kent Ketter <kent.ketter1@gmail.com>
2019-01-09 21:03:45 -0800

    Menu Complete

--- Lab02-Unit Testing/Program.cs
+++ Lab02-Unit Testing/Program.cs
@@ -2,11 +2,53 @@
 
 namespace Lab02_Unit_Testing
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Display Application Menu including logo and 4 functions
+            Menu();
+            //ATMFunction();
+            Console.ReadLine();
+
+
+        }
+        public static void Menu()
+        {
+            Console.WriteLine(@"
+    ~~~~~~~~~
+   /Automatic\   
+  /   Teller  \ 
+ /   Machine   \ 
+ ~~~~~~~~~~~~~~~~
+(1) Display Balance
+(2) Deposit Money
+(3) Withdrawl Money
+(4) Exit
+");
+        }
+        public static void MenuOptions(int menuSelection)
+        {
+            switch (menuSelection)
+            {
+                // Display Balance
+                case 0:
+                    DisplayBalance(Balance);
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                // Deposit Money
+                case 1:
+                    Console.ReadKey();
+                
+                // Exit Method
+                default:
+                    Console.Write("Apologies we didnt catch that. Please try again. \nPress any key to continue.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+            }
         }
     }
 }
+
